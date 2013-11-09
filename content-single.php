64125bb3dbc013e86471e299a24e491f4466edce
Stefan Brechbühl <email@pixelstrol.ch>
2013-11-09 18:13:50 +0100

    Fixed get_the_tag_list()

--- content-single.php
+++ content-single.php
@@ -64,7 +64,7 @@
 				$category_list = get_the_category_list( ', ' );
 	
 				/* translators: used between list items, there is a space after the comma */
-				$tag_list = get_the_tag_list( ', ' );
+				$tag_list = get_the_tag_list( '',', ' );
 	
 				if ( ! albinomouse_categorized_blog() ) {
 					// This blog only has 1 category so we just need to worry about tags in the meta text
