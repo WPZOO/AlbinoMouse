825ff8ae5a809c3cf13170daa8683f6116a54966
Stefan Brechbühl <email@pixelstrol.ch>
2015-02-23 18:55:31 +0100

    Convert indentation to tabs

--- searchform-header.php
+++ searchform-header.php
@@ -7,6 +7,6 @@
 <form role="search" method="get" id="searchform" class="navbar-form navbar-right" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div class="form-group">
 		<span class="sr-only"><?php _ex( 'Search for:', 'label', 'albinomouse' ); ?></span>
-	    <input type="text" class="search-field form-control input-lg" value="" name="s" id="s" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'albinomouse' ); ?>"/>
+			<input type="text" class="search-field form-control input-lg" value="" name="s" id="s" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'albinomouse' ); ?>"/>
 	</div>
 </form>
